e51eda26cb08cadc90a560c5d86eb7437858895a
Kazolkht <127694071+Kazolkht@users.noreply.github.com>
2023-04-03 23:00:13 -0400

    Update places.py
    We went through the older codes and worked on the last codes created to get a refined version of our final codes.(Google_place.py)

--- places.py
+++ places.py
@@ -11,3 +11,4 @@ class Place:
         print("Name: " + self.name)
         print("Google Reviews Rating: " + str(self.rating))
         print("Address: " + self.address)
+#We went through the older codes and worked on the last codes created to get a refined version of our final codes.(Google_places.py)
